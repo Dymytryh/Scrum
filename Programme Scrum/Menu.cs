2b744983daeee019af56ac27ff74f8707e1dd6d9
Dymytryh <dimiddp@hotmail.com>
2020-06-22 14:18:05 +0200

    S.U. 1

---
+++ Programme Scrum/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programme_Scrum
+{
+    class Menu
+    {
+        public static void WriteMenu(List<string> list)
+        {
+            int place = 1;
+            foreach (var element in list)
+            {
+                Console.WriteLine(place +". " + element);
+                place++;
+            }
+            int lenght = list.Count() + 1 ;
+            Console.WriteLine(lenght + ". Quitter");
+           
+        }
+
+        public static void CallMenu(Dictionary<int,Func<string>> dic, int a) 
+        {
+            dic.Add(dic.Last().Key + 1, Menu.Test);
+            var call = dic.Where(k => k.Key == a).FirstOrDefault().Value.DynamicInvoke();
+            Console.WriteLine(call);
+        }
+
+        public static string Test()
+        {
+            return "Hello there";
+        }
+    }
+}
--- Programme Scrum/Program.cs
+++ Programme Scrum/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace Programme_Scrum
     {
         static void Main(string[] args)
         {
+            List<string> menu = new List<string>(new string[] { "Nouvelle personne", "Supprimer personne"});
+            Menu.WriteMenu(menu);
+            int a = int.Parse(Console.ReadLine());
+            Dictionary<int, Func <string>> dic = new Dictionary<int, Func<string>>();
+            dic.Add(1, Menu.Test);
+            dic.Add(2, Menu.Test);
+            Menu.CallMenu(dic,a);
+            Console.ReadKey();
         }
     }
 }
